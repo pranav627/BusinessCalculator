9b34a07b9e6d58b0b21f0d1f174307cfdbed4ad0
pranav <pranav.r@dxc.com>
2023-07-16 23:58:01 +0530

    Ticket#0002-added UI and Calculation

--- business-calculator/src/app/app.module.ts
+++ business-calculator/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,8 +12,9 @@ import { CalculatorComponent } from './calculator/calculator.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- business-calculator/src/app/calculator/calculator.component.ts
+++ business-calculator/src/app/calculator/calculator.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent {
+  costs: number;
+  price: number;
+  results: number[] = [];
 
-  constructor() { }
+  calculate(): void {
+    if (!this.costs) {
+      return; // Skip calculation if no costs are provided
+    }
 
-  ngOnInit(): void {
+    const totalCosts = Array.isArray(this.costs)
+      ? this.costs.reduce((acc, curr) => acc + curr, 0)
+      : this.costs;
+
+    this.results = Array.isArray(this.costs)
+      ? this.costs.map(cost => Math.ceil(cost / (this.price - cost)))
+      : [Math.ceil(totalCosts / (this.price - totalCosts))];
   }
-
 }
